f3d55f81e19c5b55ffeb342614384c3948923442
danieelfcr <danielfcab2002@gmail.com>
2023-02-19 11:33:23 -0600

    Graphical interface designed.

--- Scanner/Scanner/Form1.Designer.cs
+++ Scanner/Scanner/Form1.Designer.cs
@@ -28,12 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnAnalyze = new System.Windows.Forms.Button();
+            this.rTBResult = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 16F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(325, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(453, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Language Scanner";
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnOpenFile.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnOpenFile.Location = new System.Drawing.Point(118, 184);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(256, 57);
+            this.btnOpenFile.TabIndex = 1;
+            this.btnOpenFile.Text = "Open file";
+            this.btnOpenFile.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(405, 189);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(597, 43);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btnAnalyze
+            // 
+            this.btnAnalyze.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnAnalyze.Enabled = false;
+            this.btnAnalyze.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnAnalyze.Location = new System.Drawing.Point(420, 294);
+            this.btnAnalyze.Name = "btnAnalyze";
+            this.btnAnalyze.Size = new System.Drawing.Size(256, 57);
+            this.btnAnalyze.TabIndex = 3;
+            this.btnAnalyze.Text = "Analyze grammar";
+            this.btnAnalyze.UseVisualStyleBackColor = false;
+            // 
+            // rTBResult
+            // 
+            this.rTBResult.Location = new System.Drawing.Point(118, 397);
+            this.rTBResult.Name = "rTBResult";
+            this.rTBResult.ReadOnly = true;
+            this.rTBResult.Size = new System.Drawing.Size(878, 279);
+            this.rTBResult.TabIndex = 4;
+            this.rTBResult.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 37F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FloralWhite;
+            this.ClientSize = new System.Drawing.Size(1144, 784);
+            this.Controls.Add(this.rTBResult);
+            this.Controls.Add(this.btnAnalyze);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Language Scanner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private Button btnOpenFile;
+        private TextBox textBox1;
+        private Button btnAnalyze;
+        private RichTextBox rTBResult;
     }
 }
