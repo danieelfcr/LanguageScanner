c618b0422d636759114aa7c2da6e30bd507f5b0d
DiegoBautista <dabautistac@correo.url.edu.gt>
2023-02-25 21:39:33 -0600

    Add RE for ACTIONS section

--- Scanner/Scanner/RegularExpressions.cs
+++ Scanner/Scanner/RegularExpressions.cs
@@ -10,7 +10,9 @@ namespace Scanner
     internal class RegularExpressions
     {
         //ER de ACTIONS--------------------------------------------------------------------------------------
-        
+        Regex actionsRE = new Regex("ACTIONS\\s*RESERVADAS\\s*\\(\\s*\\)\\s*{\\s*(([0-9]|([1-9][0-9]*))\\s*=\\s*'([A-Z]|[a-z])+'\\s*)+}\\s*(([A-Z]|[a-z])+\\s*\\(\\s*\\)\\s*{\\s*(([0-9]|([1-9][0-9]*))\\s*=\\s*'([A-Z]|[a-z])+'\\s*)+}\\s*)*");
+        Regex actionsToken = new Regex("(\\s*([0-9]|([1-9][0-9]*))\\s*=\\s*'([A-Z]|[a-z])+'\\s*)+");
+        Regex actionsFunction = new Regex("(([A-Z]|[a-z])+\\s*\\(\\s*\\)\\s*{\\s*(([0-9]|([1-9][0-9]*))\\s*=\\s*'([A-Z]|[a-z])+'\\s*)+}\\s*)*");
 
     }
 }
