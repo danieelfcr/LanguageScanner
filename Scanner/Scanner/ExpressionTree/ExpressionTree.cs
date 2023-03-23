6188f7a4cc32e690ca67ec3e489ed418e7d8b2d8
JoseMarold <jmmr114@gmail.com>
2023-03-22 21:31:58 -0600

    Expression Tree basic structure created

---
+++ Scanner/Scanner/ExpressionTree/ExpressionTree.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scanner.ExpressionTree
+{
+    public class ExpressionTree
+    {
+        public Node Root;
+        public int leafCount;
+        public int nodeCount;
+        public string[,] firstLastMatrix;
+        private Dictionary<string, List<int>> followTable; //symbol, follow list
+        
+        private Dictionary<string, int> operatorHierarchy = new Dictionary<string, int>() //symbol, number in the hierarchy
+        {
+            {"|", 1},
+            {".", 2},
+            {"*", 3},
+            {"+", 4},
+            {"?", 5},
+            {"(", 6},
+            {")", 7},
+        }; 
+
+        public ExpressionTree(string regularExpression)
+        {
+
+        }
+
+    }
+}
---
+++ Scanner/Scanner/ExpressionTree/Node.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scanner.ExpressionTree
+{
+    public class Node
+    {
+        public string symbol;
+        public Node right;
+        public Node left;
+        private List<int> firstList;
+        private List<int> lastList;
+        public bool nullable = false;
+        public int leafNumber;
+
+        public Node(string symbol, Node right, Node left, bool nullable)
+        {
+            this.symbol = symbol;
+            this.right = right;
+            this.left = left;
+            firstList = new List<int>();
+            lastList = new List<int>();
+            this.nullable = nullable;
+            leafNumber = 0;
+        }
+
+    }
+}
