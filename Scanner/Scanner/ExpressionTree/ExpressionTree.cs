2e5971a1c9af993d26c0eb3f7d02f81181afa910
JoseMarold <jmmr114@gmail.com>
2023-03-24 22:21:19 -0600

    SetConcatenations method added

--- Scanner/Scanner/ExpressionTree/ExpressionTree.cs
+++ Scanner/Scanner/ExpressionTree/ExpressionTree.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,12 +26,86 @@ namespace Scanner.ExpressionTree
             {")", 7},
         }; 
 
-        public ExpressionTree(string regularExpression)
+        public ExpressionTree(string regularExpression, Queue<Node> tokenSource)
         {
             leafCount = 0;
-            
+            Root = CreateTree(tokenSource);
             firstLastMatrix = new string[nodeCount, 4];
             followTable = new Dictionary<string, List<int>>();
+        }
+
+        public void SetConcatenations(ref Queue<Node> tokenSource)
+        {
+            Node baseToken;
+            Node nextToken;
+            int tokenSourceOriginalLenght = tokenSource.Count;
+            for (int i = 1; i <= tokenSourceOriginalLenght; i++)
+            {
+                baseToken = tokenSource.Dequeue();
+                nextToken = tokenSource.Peek();
+                bool needsConcat = false;
+
+                if (baseToken != null && nextToken != null)
+                {
+                    switch (baseToken.kindSymbol)
+                    {
+                        case 0:
+                            //terminal symbol
+                            if (nextToken.kindSymbol == 0 | nextToken.kindSymbol == 1 | nextToken.kindSymbol == 3)
+                                needsConcat = true;
+                            break;
+                        case 1:
+                            //no terminal symbol
+                            if (nextToken.kindSymbol == 0 | nextToken.kindSymbol == 1 | nextToken.kindSymbol == 3)
+                                needsConcat = true;
+                            break;
+                        case 2:
+                            //operator
+                            if (nextToken.kindSymbol == 0 | nextToken.kindSymbol == 1 | nextToken.kindSymbol == 3)
+                                needsConcat = true;
+                            break;
+                        case 3:
+                            //open parenthesis
+                            needsConcat = false;
+                            break;
+                        case 4:
+                            //closed parenthesis
+                            if (nextToken.kindSymbol == 0 | nextToken.kindSymbol == 1 | nextToken.kindSymbol == 3)
+                                needsConcat = true;
+                            break;
+                        default:
+                            needsConcat = false;
+                            break;
+                    }
+                
+                    if (needsConcat)
+                    {
+                        //concatenation required
+                        Node concat = new Node(".", 2);     //new node with concatenation operator
+                        tokenSource.Enqueue(baseToken);     //base token to the end of tokenSource
+                        tokenSource.Enqueue(concat);        //concatenation node to the end of tokenSource
+                    }
+                    else
+                    {
+                        //no concatenation required
+                        tokenSource.Enqueue(baseToken);     //new node with concatenation operator
+                    }
+                }
+            }
+        }
+
+        private Node CreateTree(Queue<Node> tokenSource)
+        {
+            SetConcatenations(ref tokenSource);
+            Stack<Node> T = new Stack<Node>(); //Stack of tokens
+            Stack<Node> S = new Stack<Node>(); //Stack of trees
+
+            while(tokenSource.Count > 0)
+            {
+                Node actualToken = tokenSource.Dequeue();
+
+            }
+
         }
 
     }
--- Scanner/Scanner/ExpressionTree/Node.cs
+++ Scanner/Scanner/ExpressionTree/Node.cs
@@ -9,6 +9,7 @@ namespace Scanner.ExpressionTree
     public class Node
     {
         public string symbol;
+        public int kindSymbol;
         public Node right;
         public Node left;
         private List<int> firstList;
@@ -16,14 +17,15 @@ namespace Scanner.ExpressionTree
         public bool nullable = false;
         public int leafNumber;
 
-        public Node(string symbol, Node right, Node left, bool nullable)
+        public Node(string symbol, int kind)
         {
             this.symbol = symbol;
-            this.right = right;
-            this.left = left;
+            kindSymbol = kind;
+            this.right = null;
+            this.left = null;
             firstList = new List<int>();
             lastList = new List<int>();
-            this.nullable = nullable;
+            this.nullable = false;
             leafNumber = 0;
         }
 
